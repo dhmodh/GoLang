be1ad09c2ad76cde1ce20baa6e4b319eae44f969
dhmodh <57029942+dhmodh@users.noreply.github.com>
2020-04-09 18:57:13 +0530

    Delete variable_scope.py

--- variable_scope.py
+++
@@ -1,7 +0,0 @@
-def my_func():
-	x = 10					#This x is local in my_func()
-	print("value inside Function:- ", x)
-	
-x = 20						#This x is defined in Global scope
-my_func()
-print("Value Outside Function:- ", x)
