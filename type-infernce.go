2c4e1177b579f0bb5aafa3a5f9c8d780bd4bfa63
dhmodh <dishantdmx2000@gmail.com>
2020-05-09 16:40:50 +0530

    When declaring a variable without specifying an explicit type either by using the := syntax or var = expression syntax

---
+++ type-infernce.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	fmt.Printf("v is of type %T\n", v)
+}
+//When declaring a variable without specifying an explicit type (either by using the := syntax or var = expression syntax), the variable's type is inferred from the value on the right hand side.
