97f65ce4dd51ffc2c2f2f022c9fefb89a1cb5eee
dhmodh <dishantdmx2000@gmail.com>
2020-05-09 16:39:48 +0530

    The expression T(v) converts the value v to the type T.

---
+++ type-conversion.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
+/*put more simply:
+
+i := 42
+f := float64(i)
+u := uint(f)*/
